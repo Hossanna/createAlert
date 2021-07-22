b984c899a26975c0faee703b92db77ccfd5d1189
Hosanna <hosabivan@gmail.com>
2021-07-22 23:09:09 +0100

    modify css

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 
+// document.getElementById("createAlert").addEventListener('click', () => {
+//     alert('button was clicked')
+// })
+
+var createAlert = document.getElementById("createAlert");
+var container = document.getElementsByClassName("toggle");
+
+createAlert.addEventListener("click", () => {
+    container.classList.toggle("toggle");
+    return createAlert;
+});
